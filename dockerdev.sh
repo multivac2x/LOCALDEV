72fdc1ed54397ee3c150ab46ccebf48a64b7260d
multivac2x <multivac2x@gmail.com>
2022-04-03 23:41:20 +0200

    Added new code

--- dockerdev.sh
+++ dockerdev.sh
@@ -1,9 +1,32 @@
 if [ -n "$1" ]; then
-  echo "You are now removing the $1container"
+  echo "You are now removing the $1 container"
 else
-  echo "The container name is not supplied."
+  echo "You should specify at least one parameter (run, pull, remove, dropdatabase)"
   exit
 fi
+
+#run container e run all
+#pull without additional parameters
+#dropdatabase database e dropdatabase all
+#remove contaienr e remove all
+
+
+
+#read -p "Enter marks: " marks
+#if [ $marks -ge 80 ]
+#then
+#  echo "Excellent"
+#elif [ $marks -ge 60 ]
+#then
+#  echo "Good"
+#else
+#  echo "Satisfactory"
+#fi
+
+#sudo docker pull mariadb
+#sudo docker pull phpmyadmin
+#sudo docker pull wordpress
+
 
 username=$USER
 
@@ -15,3 +38,7 @@ sleep 1
 sudo rm /home/$username/wordpress/$1 -rf
 sleep 1
 sudo docker exec -i wordpressdb mysql -uroot -proot-password  <<< "drop database IF EXISTS $1;"
+
+
+
+
