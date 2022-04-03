85235222e5a60969872c8986b119b3526de7efd2
multivac2x <multivac2x@gmail.com>
2022-04-03 23:15:47 +0200

    PROVA

--- dockerdev.sh
+++ dockerdev.sh
@@ -1,2 +1,17 @@
+if [ -n "$1" ]; then
+  echo "You are now removing the $1container"
+else
+  echo "The container name is not supplied."
+  exit
+fi
 
-TEST DA PHPSTORM
+username=$USER
+
+sleep 1
+sudo docker container stop $1
+sleep 1
+sudo docker container rm $1
+sleep 1
+sudo rm /home/$username/wordpress/$1 -rf
+sleep 1
+sudo docker exec -i wordpressdb mysql -uroot -proot-password  <<< "drop database IF EXISTS $1;"
